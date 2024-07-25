6d58f4e2b7588acc83062f0a7f42fb5774646f75
Keanu Reaño <reanokeanu@gmail.com>
2024-07-25 12:44:04 +0800

    update next.config.js with base path and output settings.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/artms-vinyl",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
